e324518128eb01aabf595e46e038c4a746a6206a
Kieran O'Neill <hello@kieranoneill.com>
2024-11-08 14:33:38 +0000

    feat: add boundary workspace with ssh key

---
+++ workspaces/boundary/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  # use terraform cloud to handle state
+  cloud {
+    organization = "kieranoneill"
+
+    workspaces {
+      name = "boundary"
+    }
+  }
+
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
